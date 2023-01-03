07bb13302b0828cef432b00ef5dee7bfcc940cbf
Henry-tepiaco <104455850+Henry-tepiaco@users.noreply.github.com>
2023-01-02 20:04:43 -0800

    real first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,99 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
+import Oreha from '../orehaCrafting.json'
 
 const inter = Inter({ subsets: ['latin'] })
 
+type ItemPrices = {
+  fish: number,
+  pearl: number,
+  goldFish: number,
+  blueOreha: number,
+  greenOreha: number,
+  purpleOreha: number,
+  greenPotion: number,
+  bluePotion: number,
+  purplePotion: number
+}
+
 export default function Home() {
+
+  const [discount, setDiscount] = useState(
+    {
+      special: 12,
+      battle: 0,
+      time: 16
+    }
+  )
+
+  const [price, setPrice] = useState<ItemPrices>({
+      //Fish
+      goldFish:     13,
+      pearl:        3,
+      fish:         24,
+
+      //Oreha
+      blueOreha:    11,
+      greenOreha:   9,
+      purpleOreha:  17,
+
+      //Potions
+      greenPotion:  10,
+      bluePotion:   15,
+      purplePotion: 20
+  })
+
+  const changeDiscount = (type: string, discount: number) => {
+    setDiscount((prevState) => ({ ...prevState, [type]: discount })) 
+  }
+ 
+  const changePrice = (item: string, price: number) => {
+    setPrice((prevState) => ({ ...prevState, [item]: price })) 
+  }
+ 
+  const calculateFishPrice = (item: string) => {
+    let goldFishCost  = (price.goldFish / 10)   * Oreha[item].fish.goldFish;
+    let pearlCost     = (price.pearl / 10)      * Oreha[item].fish.pearl;
+    let fishCost      = (price.fish / 100)      * Oreha[item].fish.fish;
+    let craftingCost  = Math.floor(Oreha[item].fish.costToCraft * (1 - (discount.special / 100)));
+    let totalCost     = fishCost + pearlCost + goldFishCost + craftingCost;
+
+    let timeToCraft   = Math.floor(Oreha[item].fish.timeToCraft * (1 - (discount.time / 100)));
+    let auctionPrice  = Math.floor(price[item] * 0.95) * Oreha[item].fish.qtyCrafted; //Add 1 for each additional Great Success
+    let profit        = auctionPrice - totalCost;
+    let ThreeLines    = Math.round(profit * 30);
+
+    return (
+      <>
+        <table>
+          <tbody>
+            <tr>
+              <td colSpan={2}> {item} </td>
+            </tr>
+            <tr>
+              <td>Profit per craft:</td>
+              <td>{profit.toFixed(1)}g</td>
+            </tr>  
+            <tr>
+              <td>Profit per {Oreha[item].fish.qtyCrafted}x craft:</td>
+              <td>{ThreeLines}g</td>
+            </tr>  
+            <tr>
+              <td>Got "Great Success"?</td>
+              <td>{auctionPrice}g per hit</td>
+            </tr>
+            <tr>
+              <td>Time to finish 30x:</td>
+              <td>{(((timeToCraft * 10)/60) / 60).toFixed(0)}hr {(((timeToCraft * 10) / 60) % 60).toFixed(0)}m</td>
+            </tr>
+          </tbody>
+        </table>
+      </>
+    );
+  };
+
   return (
     <>
       <Head>
@@ -15,107 +104,58 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className={styles.itemPrices}>
+          <table cellSpacing={10}>
+            <tbody>
+              <tr>
+                <td colSpan={2}> Discount </td>
+              </tr>
+              <tr>
+                <td>Special Discount</td>
+                <td><input type="number" value={discount.special} onChange={(e) => changeDiscount('special', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr><td colSpan={2}></td></tr>
+              <tr><td colSpan={2}></td></tr>
+              <tr>
+                <td colSpan={2}> Fishes </td>
+              </tr>
+              <tr>
+                <td>Fish</td>
+                <td><input type="number" value={price.fish} onChange={(e) => changePrice('fish', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr>
+                <td>Pearl</td>
+                <td><input type="number" value={price.pearl} onChange={(e) => changePrice('pearl', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr>
+                <td>Gold Fish</td>
+                <td><input type="number" value={price.goldFish} onChange={(e) => changePrice('goldFish', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr><td colSpan={2}></td></tr>
+              <tr><td colSpan={2}></td></tr>
+              <tr>
+                <td colSpan={2}> Orehas </td>
+              </tr>
+              <tr>
+                <td>Simple Oreha (Green)</td>
+                <td><input type="number" value={price.greenOreha} onChange={(e) => changePrice('greenOreha', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr>
+                <td>Basic Oreha (Blue)</td>
+                <td><input type="number" value={price.blueOreha} onChange={(e) => changePrice('blueOreha', parseInt(e.target.value))} /></td>
+              </tr>
+              <tr>
+                <td>Superior Oreha (Purple)</td>
+                <td><input type="number" value={price.purpleOreha} onChange={(e) => changePrice('purpleOreha', parseInt(e.target.value))} /></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          {calculateFishPrice('greenOreha')}
+          {calculateFishPrice('blueOreha')}
+          {calculateFishPrice('purpleOreha')}
         </div>
       </main>
     </>
